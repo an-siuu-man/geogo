8f4cad91f8e7ef014067e151a3b21daf4716a2e8
preston <prestonwehrman@gmail.com>
2024-04-13 14:02:02 -0500

    added more to dropdowns

--- geogobackend/geogo/static/geogo/index.js
+++ geogobackend/geogo/static/geogo/index.js
@@ -1,25 +1,21 @@
-//var parser = new DOMParser();
-//var map = document.getElementById("World");
-
-
 // Select the group element by its ID
-var group = document.getElementById('World');
+var map = document.getElementById('World');
 
 // Add an event listener to the group element
-group.addEventListener('click', function(event) {
+map.addEventListener('click', function (event) {
     // Access the ID of the specific SVG element that was clicked
     var svgID = event.target.id;
     // Display the ID in the console
     console.log(svgID);
 });
 
-        // Function to load the Budget Your Trip widget
-        function loadBudgetYourTripWidget() {
-            var script = document.createElement('script');
-            script.src = 'https://widget.budgetyourtrip.com/location-widget-js/2643743&hidebudgetstyles=1';
-            script.async = true;
-            document.getElementById('averageCost').appendChild(script);
-        }
+// Function to load the Budget Your Trip widget
+function loadBudgetYourTripWidget() {
+    var script = document.createElement('script');
+    script.src = 'https://widget.budgetyourtrip.com/location-widget-js/2643743&hidebudgetstyles=1';
+    script.async = true;
+    document.getElementById('averageCost').appendChild(script);
+}
 
-        // Call the function to load the widget
-        loadBudgetYourTripWidget();
+// Call the function to load the widget
+loadBudgetYourTripWidget();
