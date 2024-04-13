e604a39de555b54e8377bf879a6f11cc39585a11
an-siuu-man <ansuman_sharma@protonmail.com>
2024-04-13 06:21:25 -0500

    added starter template files

---
+++ geogobackend/geogo/api/gptAPI.py
@@ -0,0 +1,24 @@
+import requests
+import os
+
+def generateResponse(prompt):
+
+    # OpenAI API endpoint
+    api_endpoint = 'https://api.openai.com/v1/chat/completions'
+
+    # Request headers
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f"Bearer {os.environ.get('OPENAI_API_KEY')}"
+    }
+
+    # Request payload
+    payload1 = {
+    'model': 'gpt-4',
+    'messages': [{'role': 'user', 'content': prompt}],
+    'temperature': 0.8
+    }
+
+    response1 = requests.post(api_endpoint, json=payload1, headers=headers)
+    l = response1.json()['choices'][0]['message']['content']
+    return l
--- geogobackend/geogo/views.py
+++ geogobackend/geogo/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django import forms
+from .api.gptAPI import generateResponse
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'geogo/homepage.html')
+
+def results(request):
+    
+    response = generateResponse('top 10 tourist locations in Paris in a list with one line descriptions').split('\n')
+
+    parsedResponse = []
+    for i in response:
+        if i != '':
+            parsedResponse.append(i)
+
+    return render(request, 'geogo/results.html', {
+        'response': parsedResponse
+    })
