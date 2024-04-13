901cc59c8586fff4d8083c031cd04e883947d7c4
Ronald Heminway <ronald.heminway@ku.edu>
2024-04-13 11:24:35 -0500

    editing js

--- geogobackend/geogo/static/geogo/index.js
+++ geogobackend/geogo/static/geogo/index.js
@@ -1,2 +1,14 @@
-var parser = new DOMParser();
-var map = document.getElementById("World");
+//var parser = new DOMParser();
+//var map = document.getElementById("World");
+
+
+// Select the group element by its ID
+var group = document.getElementById('World');
+
+// Add an event listener to the group element
+group.addEventListener('click', function(event) {
+    // Access the ID of the specific SVG element that was clicked
+    var svgID = event.target.id;
+    // Display the ID in the console
+    console.log(svgID);
+});
