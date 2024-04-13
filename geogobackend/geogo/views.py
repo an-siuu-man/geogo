6ed107475044d4072757ab505fe14fba62d83661
an-siuu-man <ansuman_sharma@protonmail.com>
2024-04-13 14:01:25 -0500

    added error logic to homepage form

--- geogobackend/geogo/views.py
+++ geogobackend/geogo/views.py
@@ -12,7 +12,7 @@ def results(request):
 
         originAirport = request.POST.get('originAirport')
         destinationAirport = request.POST.get('destinationAirport')
-        
+        getVisa = request.POST.get('getVisa')
         # You can now use the selected option as needed
         if originAirport == 'MCI':
             origin = 'Kansas City'
@@ -32,9 +32,29 @@ def results(request):
         else:
             destination = 'Unknown'
 
+        if getVisa == 'MCI':
+            visa = 'Kansas City'
+        elif getVisa == 'LHR':
+            visa = 'London'
+        elif getVisa == 'ORD':
+            visa = 'Chicago'
+        else:
+            visa = 'Unknown'
+
+        same_airport = False
+        invalid_airport = False
+        invalid_visa = False
+
+        # Error Case 1 
         if origin == destination:
+            same_airport = True
+            invalid_airport = False
+            invalid_visa = False
+
             return render(request, 'geogo/index.html', {
-                'same_airport': True
+                'same_airport': same_airport,
+                'invalid_airport':invalid_airport,
+                'invalid_visa': invalid_visa
                 })
 
 
@@ -60,15 +80,34 @@ def results(request):
     "Sainte-Chapelle: Gothic chapel known for its stunning stained glass windows.",
     "Musée Rodin: Museum showcasing the works of the French sculptor Auguste Rodin, set in a mansion with gardens."
 ]
-        if origin != 'Unknown' or destination != 'Unknown':
-            return render(request, 'geogo/results.html', {
+        
+        # Error Case 2
+        if origin == 'Unknown' or destination == 'Unknown':
+            invalid_airport = True
+            same_airport = False
+            invalid_visa = False
+            return render(request, 'geogo/index.html', {
+                'invalid_airport':invalid_airport,
+                'same_airport':same_airport,
+                'invalid_visa':invalid_visa
+                })
+        
+        # Error Case 3
+        if visa == 'Unknown':
+            invalid_visa = True
+            invalid_airport = False
+            same_airport = False
+            return render(request, 'geogo/index.html', {
+                'invalid_airport':invalid_airport,
+                'same_airport':same_airport,
+                'invalid_visa':invalid_visa
+                })
+
+        # no error case
+        return render(request, 'geogo/results.html', {
             'locations': locations, 
             'origin':origin,
-            'destination':destination
+            'destination':destination,
+            'getVisa': visa
         })
-
-        else:
-            return render(request, 'geogo/index.html', {
-                'invalid_airport':True
-                })
     
